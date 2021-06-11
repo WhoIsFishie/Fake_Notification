bb990cffc0e96669900efc61e6dde5343abc6f47
CEO <CEO@REDDESKTOP>
2021-06-11 20:48:17 +0500

    Added Comments

--- app/src/main/java/com/fishie/fakenotification/MainActivity.kt
+++ app/src/main/java/com/fishie/fakenotification/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //idk what this does
+        //all i know is as long as it exists the code is fine so let it be
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create the NotificationChannel
             val name = "gobama"
@@ -30,23 +32,34 @@ class MainActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(mChannel)
         }
 
+        //init
         val btn_click_me = findViewById(R.id.caculate) as Button;
         var Title = findViewById(R.id.title) as EditText;
         var Notif = findViewById(R.id.content) as EditText;
+        //end init
+
+
         // set on-click listener
         var textTitle = Title.text;
         var textContent = Notif.text;
         btn_click_me.setOnClickListener {
-            var builder = NotificationCompat.Builder(this,"obama")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(textTitle)
-                .setContentText(textContent)
+
+            //construct notif
+            var builder = NotificationCompat.Builder(this, "obama")
+                .setSmallIcon(R.drawable.ic_launcher_background) //this can be customized in the future if i feel like it
+                .setContentTitle(textTitle) //assign title
+                .setContentText(textContent) //assign content
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            val notification_id = Random()
+
+            val notification_id = Random() //give custom id so app can make many Notification
+
+            //show Notification
             with(NotificationManagerCompat.from(this)) {
                 // notificationId is a unique int for each notification that you must define
                 notify(notification_id.nextInt(100), builder.build())
             }
+
+            //debug
             Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show();
         }
     }
